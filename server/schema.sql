6a49fc7643f9a2fa4725f60e4ed2025cb7238709
Jadennnnnnn <jadenjee@gmail.com>
2022-09-28 16:18:06 -0700

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -4,11 +4,19 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  ID int(10) PRIMARY KEY,
+  message_text varchar(255),
+  username_id int(10),
+  roomname varchar(255)
 );
 
 /* Create other tables and define schemas for them here! */
 
-
+CREATE TABLE users (
+  /* Describe your table here.*/
+  ID int(10) PRIMARY KEY,
+  name varchar(255)
+);
 
 
 /*  Execute this file from the command line by typing:
